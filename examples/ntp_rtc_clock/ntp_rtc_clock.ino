bbcf7720bf6a2d0ac6d8414513b2fb54355cb15c
JChristensen <jack.christensen@outlook.com>
2025-06-24 11:34:59 -0400

    Update example sketch to detect RTC type, DS323x or MCP7941x.

--- examples/ntp_rtc_clock/ntp_rtc_clock.ino
+++ examples/ntp_rtc_clock/ntp_rtc_clock.ino
@@ -1,18 +1,20 @@
 // An NTP and RTC clock that can set the RTC time from NTP.
 // Press and release the button to switch between display modes.
 // Long press to set the RTC time from NTP.
-// Wifi credentials are stored in EEPROM. If credientials were not saved
+// Wifi credentials are stored in the Pico's EEPROM. If credientials were not saved
 // previously, then the sketch will prompt for them.
 // Hold the button down while resetting the MCU to change wifi credentials.
-// Time from MCP7941x RTC on i2c1 (Wire1) using 1Hz interrupt.
+// Works with either DS323x or MCP7941x RTC; type detected automatically.
+// Time from RTC on i2c1 (Wire1) using 1Hz interrupt.
 // Display on SSD1306 OLED on i2c0 (Wire).
 // For RPi Pico.
 // J.Christensen 13Jun2025
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <DS3232RTC.h>          // https://github.com/JChristensen/DS3232RTC
 #include <JC_Button.h>          // https://github.com/JChristensen/JC_Button
-#include <MCP79412RTC.h>        // https://github.com/JChristensen/DS3232RTC
+#include <MCP79412RTC.h>        // https://github.com/JChristensen/MCP79412RTC
 #include <PicoWifiManager.h>    // https://github.com/JChristensen/PicoWifiManager
 #include <Streaming.h>          // https://github.com/janelia-arduino/Streaming
 #include <Timezone.h>           // https://github.com/JChristensen/Timezone
@@ -20,20 +22,22 @@
 // constant parameters
 constexpr int wireSdaPin {12}, wireSclPin {13};     // I2C pins for Wire
 constexpr int wire1SdaPin {26}, wire1SclPin {27};   // I2C pins for Wire1
+constexpr uint8_t ds_addr {0x68}, mcp_addr {0x6f};
 constexpr int wifiLED {7};          // illuminates to indicate wifi connected
 constexpr int btnPin {14};          // display select/RTC set
 constexpr int RTC_1HZ_PIN {22};
 constexpr int OLED_WIDTH {128};     // OLED display width, in pixels
 constexpr int OLED_HEIGHT {64};     // OLED display height, in pixels
 constexpr int OLED_ADDRESS {0x3c};  // OLED I2C address
-constexpr uint8_t RTC_SET_ADDR{60}; // SRAM address in the RTC where the set time is saved
+constexpr uint8_t RTC_SET_ADDR{92}; // MCP7941x SRAM address where the set time is saved
 
 // object instantiations and globals
-HardwareSerial& mySerial {Serial};         // choose Serial, Serial1 or Serial2 here
+HardwareSerial& mySerial {Serial};          // choose Serial, Serial1 or Serial2 here
 PicoWifiManager wifi(mySerial);
 Adafruit_SSD1306 oled(OLED_WIDTH, OLED_HEIGHT, &Wire);
-MCP79412RTC myRTC(Wire1);
+GenericRTC* rtc;
 Button btn(btnPin);
+bool rtcIsDS{false}, rtcIsMCP{false};
 
 void setup()
 {
@@ -62,46 +66,10 @@ void setup()
     Wire1.setSDA(wire1SdaPin); Wire1.setSCL(wire1SclPin); Wire1.setClock(400000);
     pinMode(RTC_1HZ_PIN, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(RTC_1HZ_PIN), incrementTime, FALLING);
-    myRTC.begin();
-    if (!myRTC.isRunning()) myRTC.set(0);       // start the rtc if not running
-    myRTC.squareWave(MCP79412RTC::SQWAVE_1_HZ); // 1 Hz square wave
 
-    // print the RTC ID
-    // 8-byte RTC "unique ID" with access to upper and lower halves
-    union {
-        uint8_t b[8];
-        struct {
-            uint32_t hi;
-            uint32_t lo;
-        };
-    } rtcID;
-    myRTC.idRead(rtcID.b);
-    mySerial << "RTC ID: ";
-    for (int i=0; i<8; i++) mySerial << (rtcID.b[i] < 16 ? "0" : "" ) << _HEX(rtcID.b[i]);
-    mySerial << "\n";
-
-    // set the RTC calibration from EEPROM if present, else just print current calibration value
-    if (myRTC.eepromRead(125) == 0xAA && myRTC.eepromRead(126) == 0x55) {
-        int8_t cal = static_cast<int8_t>(myRTC.eepromRead(127));
-        myRTC.calibWrite(cal);  // set calibration register from stored value
-        mySerial.printf("RTC calibration value: %d (set from RTC EEPROM)\n", cal);
-    }
-    else {
-        int8_t cal = myRTC.calibRead();
-        mySerial.printf("RTC calibration value: %d\n", cal);
-    }
-
-    // print the time the RTC was last set (from RTC SRAM)
-    time_t lastSet = read32(RTC_SET_ADDR);
-    mySerial.printf("RTC was last set at: %.4d-%.2d-%.2d %.2d:%.2d:%.2d UTC\n",
-        year(lastSet), month(lastSet), day(lastSet),
-        hour(lastSet), minute(lastSet), second(lastSet));    
-
-    time_t utc = getUTC();                  // synchronize with RTC
-    while ( utc == getUTC() );              // wait for increment to the next second
-    utc = myRTC.get();                      // get the time from the RTC
-    setUTC(utc);                            // set our time to the RTC's time
-    mySerial << "RTC sync\n";
+    // determine which type of RTC we have and perform appropriate setup
+    Wire1.begin();
+    rtc = findRTC();
 
     // check to see if the user wants to enter new wifi credentials, else initialize wifi.
     btn.read();
@@ -184,17 +152,93 @@ void loop()
     }
 }
 
+GenericRTC* findRTC()
+{
+    Wire1.beginTransmission(ds_addr);
+    if (Wire1.endTransmission() == 0) {
+        Serial << "Found DS323x RTC\n";
+        DS3232RTC* ds_rtc{ new DS3232RTC{Wire1} };
+        ds_rtc->begin();
+        ds_rtc->squareWave(DS3232RTC::SQWAVE_1_HZ);   // 1 Hz square wave
+        rtcIsDS = true;
+
+        time_t utc = getUTC();          // synchronize with RTC
+        while ( utc == getUTC() );      // wait for increment to the next second
+        utc = ds_rtc->get();            // get the time from the RTC
+        setUTC(utc);                    // set our time to the RTC's time
+        mySerial << "RTC sync\n";
+        return ds_rtc;
+    }
+
+    Wire1.beginTransmission(mcp_addr);
+    if (Wire1.endTransmission() == 0) {
+        Serial << "Found MCP7941x RTC\n";
+        MCP79412RTC* mcp_rtc{ new MCP79412RTC{Wire1} };
+        mcp_rtc->begin();
+        if (!mcp_rtc->isRunning()) mcp_rtc->set(0);        // start the rtc if not running
+        mcp_rtc->squareWave(MCP79412RTC::SQWAVE_1_HZ); // 1 Hz square wave
+
+        // print the RTC ID
+        // 8-byte RTC "unique ID" with access to upper and lower halves
+        union {
+            uint8_t b[8];
+            struct {
+                uint32_t hi;
+                uint32_t lo;
+            };
+        } rtcID;
+        mcp_rtc->idRead(rtcID.b);
+        mySerial << "RTC ID: ";
+        for (int i=0; i<8; i++) mySerial << (rtcID.b[i] < 16 ? "0" : "" ) << _HEX(rtcID.b[i]);
+        mySerial << "\n";
+
+        // set the RTC calibration from EEPROM if present, else just print current calibration value
+        if (mcp_rtc->eepromRead(125) == 0xAA && mcp_rtc->eepromRead(126) == 0x55) {
+            int8_t cal = static_cast<int8_t>(mcp_rtc->eepromRead(127));
+            mcp_rtc->calibWrite(cal);   // set calibration register from stored value
+            mySerial.printf("RTC calibration value: %d (set from RTC EEPROM)\n", cal);
+        }
+        else {
+            mySerial.printf("RTC calibration value: %d\n", mcp_rtc->calibRead());
+        }
+    
+        // print the time the RTC was last set (from RTC SRAM)
+        union {uint8_t b[4]; time_t t;} lastSet;    // union to access a time_t byte by byte
+        mcp_rtc->readRTC(RTC_SET_ADDR, lastSet.b, 4);        
+        mySerial.printf("RTC was last set at: %.4d-%.2d-%.2d %.2d:%.2d:%.2d UTC\n",
+            year(lastSet.t), month(lastSet.t), day(lastSet.t),
+            hour(lastSet.t), minute(lastSet.t), second(lastSet.t));    
+    
+        time_t utc = getUTC();          // synchronize with RTC
+        while ( utc == getUTC() );      // wait for increment to the next second
+        utc = mcp_rtc->get();           // get the time from the RTC
+        setUTC(utc);                    // set our time to the RTC's time
+        mySerial << "RTC sync\n";
+
+        rtcIsMCP = true;
+        return mcp_rtc;
+    }
+
+    if (!rtcIsDS && !rtcIsMCP) {
+        Serial << "Error: No RTC or unknown RTC type.\n";
+        while (true);
+    }
+    return nullptr; // will never get here but we have to keep the compiler happy
+}
+
 // set the RTC from current NTP time
 void setRTC()
 {
     time_t ntpNow = time(nullptr);
-    time_t t;
-    while ((t=time(nullptr)) == ntpNow);    // wait for the next second to roll over
-    myRTC.set(t);
-    setUTC(t);
-    write32(RTC_SET_ADDR, t);   //save the utc when time set in sram
+    union {uint8_t b[4]; time_t t;} t;      // union to access a time_t byte by byte
+    while ((t.t=time(nullptr)) == ntpNow);  // wait for the next second to roll over
+    rtc->set(t.t);
+    setUTC(t.t);
+    if (rtcIsMCP) {
+        rtc->writeRTC(RTC_SET_ADDR, t.b, 4);    // //save the utc when time set in sram
+    }
     mySerial.printf("RTC set to %.4d-%.2d-%.2d %.2d:%.2d:%.2d UTC\n",
-        year(t), month(t), day(t), hour(t), minute(t), second(t));
+        year(t.t), month(t.t), day(t.t), hour(t.t), minute(t.t), second(t.t));
 }
 
 // update oled display
@@ -275,26 +319,4 @@ void setUTC(time_t utc)
 void incrementTime()
 {
     ++isrUTC;
-}
-
-// union to access a time_t byte by byte
-union timeBytes_t {
-    uint8_t b[4];
-    uint32_t t;
-};
-
-// write a time_t or other uint32_t value to sram starting at addr
-void write32(uint8_t addr, uint32_t t)
-{
-    timeBytes_t i;
-    i.t = t;
-    myRTC.sramWrite(addr, i.b, 4);
-}
-
-// read a time_t or other uint32_t value from sram starting at addr
-time_t read32(uint8_t addr)
-{
-    timeBytes_t i;
-    myRTC.sramRead(addr, i.b, 4);
-    return i.t;
 }
