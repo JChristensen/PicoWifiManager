ed8a5a91a732121d1d948486ebbea85008bc07f0
JChristensen <jack.christensen@outlook.com>
2025-07-24 15:50:57 -0400

    Add example sketch.

---
+++ examples/wifi_get/wifi_get.ino
@@ -0,0 +1,94 @@
+// A very basic sketch to send an HTTP GET request for a web page.
+// Output is on Serial2 (aka UART-1).
+// For Raspbery Pi Pico W or 2W.
+// J.Christensen 24Jul2025
+
+#include <WiFi.h>
+#include <PicoWifiManager.h>        // https://github.com/JChristensen/PicoWifiManager
+#include <TimeLib.h>
+
+// object instantiations and globals
+HardwareSerial& mySerial {Serial2}; // choose Serial, Serial1 or Serial2 here
+constexpr int txPin {4}, rxPin {5};
+PicoWifiManager wifi(mySerial);
+
+void setup()
+{
+    Serial2.setRX(rxPin); Serial2.setTX(txPin);
+    mySerial.begin(115200);
+    while (!mySerial && millis() < 2000) delay(50);
+    mySerial.printf("\n%s\nCompiled %s %s %s @ %d MHz\n",
+        __FILE__, __DATE__, __TIME__, BOARD_NAME, F_CPU / 1000000);
+    wifi.begin();
+}
+
+void loop()
+{
+    const char* host = "wttr.in";
+    const uint16_t port = 80;
+    constexpr uint32_t getInterval {300000}, retryInterval {10000}, respTimeout {10000};
+
+    enum states_t {CONNECT, WAIT_RETRY, WAIT_RECV, PRINT_RESPONSE, WAIT_CONNECT};
+    static states_t state {CONNECT};
+    static WiFiClient client;
+    static uint32_t msTimer, msLastConnect;
+    uint32_t msNow = millis();
+
+    if (wifi.run()) {
+        switch (state) {
+            case CONNECT:
+                if (client.connect(host, port)) {
+                    state = WAIT_RECV;
+                    time_t t = time(nullptr);
+                    mySerial.printf("\n%d %.4d-%.2d-%.2d %.2d:%.2d:%.2d ",
+                                    msNow, year(t), month(t), day(t), hour(t), minute(t), second(t));
+                    mySerial.printf("Sending request to %s ...\n", host);
+                    client.printf(
+                        "GET / HTTP/1.1\r\n"
+                        "Host: %s\r\n"
+                        "User-Agent: curl/8.5.0\r\n"
+                        "Accept: */*\r\n\r\n", host);
+                    msTimer = msNow;
+                    msLastConnect = msNow;
+                }
+                else {
+                    state = WAIT_RETRY;
+                    msTimer = msNow;
+                    mySerial.printf("%d Connect failed, wait for retry...\n", msNow);
+                }
+                break;
+
+            case WAIT_RETRY:
+                if (msNow - msTimer >= retryInterval) state = CONNECT;
+                break;
+
+            case WAIT_RECV:
+                if (client.available() > 0) {
+                    state = PRINT_RESPONSE;
+                    mySerial.printf("%d Receiving from server...\n", msNow);
+                }
+                else if (msNow - msTimer >= respTimeout) {
+                    client.stop();
+                    state = CONNECT;
+                    mySerial.printf("%d Response timeout!\n", msNow);
+                }
+                break;
+
+            case PRINT_RESPONSE:
+                if (client.available()) {
+                    char ch = static_cast<char>(client.read());
+                    mySerial.print(ch);
+                }
+                else {
+                    state = WAIT_CONNECT;
+                    mySerial.printf("%d Closing connection.\n", msNow);
+                    client.stop();
+                }
+                break;
+
+            case WAIT_CONNECT:
+                if (msNow - msLastConnect >= getInterval) state = CONNECT;
+                break;
+        }
+    }
+}
